7d17ab5270f840bf8cb7a75ede9d75cfccc0fa38
Darkmx10 <josuepjmx@gmail.com>
2023-09-02 13:49:02 -0600

    useTodo agregado

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,28 @@
+
+export const todoReducer = (initialState = [], action) => {
+
+    switch ( action.type ) {
+        case '[TODO] Add Todo':
+            return [ ...initialState, action.payload ];
+
+            case '[TODO] Remove Todo':
+                return initialState.filter( todo => todo.id !== action.payload ); //<--Regresa los 'todos' cuando todoReducer sea diferente a al id que recibe
+    
+                case '[TODO] Toggle Todo':
+                    return initialState.map( todo => {
+                        
+                        if( todo.id === action.payload ){
+                            return {
+                                ...todo,
+                                done: !todo.done
+                            }
+                        }
+
+                        return todo;
+                    }); 
+
+        default:
+            return initialState;
+    }
+
+}
